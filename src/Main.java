0481c4f022618e6f6bed02005b2bc55e3342ed95
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-08-30 17:43:35 +0300

    hw3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 8;
-        if (age >= 7 && age < 18) {System.out.println("Ходит в школу.");}
-        if (age >= 18 && age < 24) {System.out.println("Ходит в университет.");}
-        if (age >= 24) {System.out.println("Пора искать работу.");}
+        int passenger = 10;
+        if (passenger < 60) {System.out.println("Есть сидячие и стоячие места.");}
+        if (passenger >= 60 && passenger< 102) {System.out.println("Есть стоячие места.");}
+        if (passenger >= 102) {System.out.println("Мест нет.");}
     }
 }
