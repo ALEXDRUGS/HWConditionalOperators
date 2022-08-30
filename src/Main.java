251dc52aaa42fce0bb82b963fddbabf915447d10
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-08-30 16:07:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 19;
+        if (age >= 18){System.out.println("Поздравляем вас с совершеннолетием!");}
+        else {System.out.println("Вы не достигли совершеннолетия, нужно подождать.");}
+    }
+}
