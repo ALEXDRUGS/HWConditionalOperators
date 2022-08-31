51e213e6ef995b675deecab87a7ae9ebee95bae0
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-08-31 14:51:37 +0300

    hw4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 18;
+        int age = 19;
         if (age >=2 && age <= 6) {System.out.println("Если возраст " + age + " лет, он ходит в детский сад.");}
         if (age >= 7 && age <= 18) {System.out.println("Если возраст " + age + " лет, он ходит в школу.");}
         if (age > 18 && age <= 24) {System.out.println("Если возраст " + age + " лет, он ходит в университет.");}
