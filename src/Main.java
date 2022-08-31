6990864b7e58276810915269cb56a89a535c74ce
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-08-31 15:48:57 +0300

    hw5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 14;
+        int age = 13;
         if (age <= 5) {System.out.println("Кататься нельзя.");}
         else if (age > 5 && age < 14) {System.out.println("Кататься в сопровождении взрослого.");}
         else {System.out.println("Можно кататься одному.");}
