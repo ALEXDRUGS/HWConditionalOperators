d1ad3fb4ef353053c1cc5ca44e90a8df090ff016
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-09-04 13:33:43 +0300

    hw9

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int clientDeviseYear = 2015;
-        int clientOS = 1;
-        int andr = 0;
-        int ios = 1;
-        if (clientDeviseYear < 2015 && clientOS == andr){
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }if (clientDeviseYear >= 2015 && clientOS == andr){
-            System.out.println("Установите приложение для Android по ссылке");}
-        else if (clientDeviseYear < 2015 && clientOS == ios){
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }if (clientDeviseYear >= 2015 && clientOS == ios){
-            System.out.println("Установите приложение для iOS по ссылке");}
+        int year = 2021;
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");}
         }
     }
 
