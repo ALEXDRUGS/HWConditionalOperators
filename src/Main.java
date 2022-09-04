f4747af93eaf639da97b7a2b4927e8a0c18ceb65
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-09-04 15:32:26 +0300

    hw10

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int year = 2021;
-        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");}
-        }
+        int deliveryDistance = 95;
+        int min = 20;
+        int i = 40; //интервал расстояния
+        int day = 1;
+        int sum = deliveryDistance / i;
+        if (deliveryDistance <= min){
+            System.out.println("Потребуется дней: " + day);}
+        else if (deliveryDistance % i <= min){
+            System.out.println("Потребуется дней: " + (sum + day));}
+        else if (deliveryDistance % i > min){
+            System.out.println("Потребуется дней: " + (sum + (day + day)));}
     }
-
-
+}
 
