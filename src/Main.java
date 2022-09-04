2746311133ae005966c2baa0ef43a2a15666dc37
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-09-04 13:07:25 +0300

    hw8

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int clientOS = 0;
-        int ios = 0;
-        int andr = 1;
-        if (clientOS == andr){System.out.println("Установите приложение для Android перейдя по ссылке");}
-        if (clientOS == ios) {System.out.println("Установите приложение для iOS перейдя по ссылке");}
+        int clientDeviseYear = 2015;
+        int clientOS = 1;
+        int andr = 0;
+        int ios = 1;
+        if (clientDeviseYear < 2015 && clientOS == andr){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }if (clientDeviseYear >= 2015 && clientOS == andr){
+            System.out.println("Установите приложение для Android по ссылке");}
+        else if (clientDeviseYear < 2015 && clientOS == ios){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }if (clientDeviseYear >= 2015 && clientOS == ios){
+            System.out.println("Установите приложение для iOS по ссылке");}
         }
     }
 
