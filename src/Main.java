d59028cc474b79fe354541cbb2a137ad27fc1e1a
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-09-09 20:08:40 +0300

    circl6

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int mounth;
         int sumAccumulation = 29_000;
-        for (mounth = 1; mounth <= 12; mounth++) {
+        mounth = 1;
+        while (sumAccumulation <= 2_500_000) {
             System.out.println(mounth + "й месяц накоплено: " + sumAccumulation);
             sumAccumulation = sumAccumulation + (29_000 + (sumAccumulation / 100));
+            mounth++;
         }
     }
 }
