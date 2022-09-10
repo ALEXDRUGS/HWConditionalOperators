e41ad4b5ba6fa8269360c8d78aadd5b6886f675e
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-09-10 08:48:00 +0300

    circl9

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int population = 12_000_000;
-        int partPopulation = 1_000;
-        int fertility = 17 * (population / partPopulation);
-        int mortality = 8 * (population / partPopulation);
-        int growthToYear = (fertility - mortality);
-        int sumGrowth = population + growthToYear;
-        int year;
-        for (year = 1; year <= 10; year++) {
-            System.out.println(year + "й год прирост населения: " + sumGrowth);
-            sumGrowth = sumGrowth + growthToYear;
+        int deposition = 15_000;
+        int percentOfDeposit = deposition / 100 * 7;
+        int mounth = 1;
+        int sumAccumulation = deposition + percentOfDeposit;
+        while (sumAccumulation <= 12_006_000) {
+            System.out.println(mounth + "й месяц накоплено:" + sumAccumulation);
+            sumAccumulation = sumAccumulation + (deposition + percentOfDeposit);
+            mounth++;
         }
     }
 }
