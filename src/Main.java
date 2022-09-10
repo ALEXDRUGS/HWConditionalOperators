a42a10d8cb2882d84db88e575136bf0ed62959c2
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-09-10 13:07:33 +0300

    circl14

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int interval;
-        int date = 2096;
-        int min = date - 200;
-        int max = date + 100;
-        for (interval = min; interval <= max; interval += 79)
-            System.out.println(interval);
+        int num = 1;
+        int num1 = 2;
+        int num2 = 2;
+        int sum = num1 * num;
+        for (num = 1; num <= 10; num++) {
+            System.out.println("2 * " + num + " = " + sum);
+            sum = num1 * num2;
+            num2++;
+        }
     }
 }
 
