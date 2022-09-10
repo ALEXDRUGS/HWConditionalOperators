b154c51812715a2ebf1123c35d568eb87705f1be
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-09-10 10:18:37 +0300

    circl11

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,15 @@ public class Main {
         int deposition = 15_000;
         int percentOfDeposit = deposition / 100 * 7;
         int mounth = 1;
+        int year = mounth * 12;
+        int dete = year * 9;
         int sumAccumulation = deposition + percentOfDeposit;
         int sumAllAccumulation = sumAccumulation / 100 * 7 + sumAccumulation;
-        while (sumAllAccumulation <= 12_039_000) {
+        for (mounth = 1; mounth <= dete; mounth++) {
             if (mounth % 6 == 0) {
                 System.out.println(mounth + "й месяц накоплено:" + sumAllAccumulation);
             }
             sumAllAccumulation += (deposition + percentOfDeposit);
-            mounth++;
         }
     }
 }
