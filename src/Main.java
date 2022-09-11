aff485b4fdf9a57ce9635333118825383a1e7ad2
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-09-11 19:43:49 +0300

    circl14Rework

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int num = 1;
-        int num1 = 2;
-        int num2 = 2;
-        int sum = num1 * num;
-        for (num = 1; num <= 10; num++) {
-            System.out.println("2 * " + num + " = " + sum);
-            sum = num1 * num2;
-            num2++;
-        }
+        int num;
+        int numConst = 2;
+        for (num = 1; num <= 10; num++)
+            System.out.println("2 * " + num + " = " + numConst * num);
     }
 }
 
